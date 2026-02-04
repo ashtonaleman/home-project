271ecd9ee0ad9e77d8df000f115c9b0a2f47f6ba
ashton <ashtonaleman@csu.fullerton.edu>
2026-02-04 15:03:35 -0800

    new stuff

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 int main()
 {
    std::cout << 5;
-   std::cout << "can you hear me";
+   std::cout << "can you hear me" endl;
    std::cout << 'H'; //this is just a symbol single qoutes
    std::cout << 7;
    std::cout << "SIX SEVEN";
